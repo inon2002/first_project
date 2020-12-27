0bcb5c1389947c0ae33908ed99edfe8d5a30e97c
inon2002 <76527864+inon2002@users.noreply.github.com>
2020-12-27 13:18:39 +0200

    Add files via upload

---
+++ LAB4.py
@@ -0,0 +1,11 @@
+id_list = ['Ynon Assaf' , 18 , 'inonhbui2002@gmail.com' , '0549948398']
+
+print("Full name: " + id_list[0] + "\nAge: " + str(id_list[1]) + "\nMail: " + id_list[2] + "\nPhone: " + id_list[3])
+
+ip_list = ["1.1.1.1" , "2.2.2.2"]
+ip_list.append("3.3.3.3")
+ip_list.append("4.4.4.4")
+ip_list.append("5.5.5.5")
+ip_list.pop(2)
+
+print("IP list length is: " + str(len(ip_list)) + "\nAnd the IP list is: " + str(ip_list))
